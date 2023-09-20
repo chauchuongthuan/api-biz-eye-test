ab5ef34fe6d777407cf0a69d82b6e1b8af470272
chauchuongthuan <chauchuongthuan@gmail.com>
2023-09-20 10:42:07 +0700

    [UPDATE] transform post award api

--- src/common/award/frontend/fePostAward.controller.ts
+++ src/common/award/frontend/fePostAward.controller.ts
@@ -2,18 +2,16 @@ import { Controller, Get, Query, Param, UseInterceptors, applyDecorators } from 
 import { ResponseService } from '@core/services/response.service';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CoreTransformInterceptor } from '@core/interceptors/coreTransform.interceptor';
-import { saveFileContent } from '@core/helpers/content';
-import { AwardService } from '../services/award.service';
-import { TransformerAwardService } from '../services/transformerAward.service';
 import { DefaultListQuery } from '@src/core/decorators/defaultListQuery.decorator';
 import { AwardPostService } from '../services/postAward.service';
+import { TransformerPostAwardService } from '../services/transformerPostAward.service';
 @ApiTags('Frontend/post-award')
 @Controller('post-award')
 @UseInterceptors(CoreTransformInterceptor)
 export class FeAwardPostController {
    constructor(
       private awardService: AwardPostService,
-      private transformer: TransformerAwardService,
+      private transformer: TransformerPostAwardService,
       private response: ResponseService,
    ) {}
 
@@ -44,6 +42,6 @@ export class FeAwardPostController {
       const item = await this.awardService.findBySlug(slug);
 
       if (!item) return this.response.detailFail();
-      return this.response.detailSuccess(await this.transformer.transformAwarFrontend(item));
+      return this.response.detailSuccess(await this.transformer.transformAwardetail(item));
    }
 }
